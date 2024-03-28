16c12cd2acb912cc6cf3f27709b1bc667d2d21af
VikasSahu1998 <sahuvikas1998@gmail.com>
2024-03-28 10:20:18 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderSideNavComponent } from './Shared/header-side-nav/header-side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderSideNavComponent
   ],
   imports: [
     BrowserModule,
